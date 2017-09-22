07e65c2366aaaa7463997420bbb62f87b9095763
Johnny Zhang <29001336@qq.com>
2017-09-22 17:49:57 +0800

    split ui with service

---
+++ app/src/main/java/com/johnny/simplelocationdemo/SimpleLocationDemoActivity.java
@@ -0,0 +1,134 @@
+package com.johnny.simplelocationdemo;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+/**
+ * Created by admin on 9/22/2017.
+ */
+
+public class SimpleLocationDemoActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg);
+
+        getUsers();
+    }
+
+    public void getUsers(){
+        new SimpleLocationDemoActivity.GetClass(this).execute();
+    }
+
+    private class GetClass extends AsyncTask<String, Void, Void> {
+        private ProgressDialog progress = new ProgressDialog(SimpleLocationDemoActivity.this);
+        private final Context context;
+        InputStream inputStream = null;
+        String result = "";
+
+        public GetClass(Context c){
+            this.context = c;
+        }
+
+        protected void onPreExecute(){
+            //progress= new ProgressDialog(this.context);
+            progress.setMessage("Loading");
+            progress.show();
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+
+                URL url = new URL("https://simple-location-demo.herokuapp.com/users");
+
+                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+//                String urlParameters = "fizz=buzz";
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty("USER-AGENT", "Mozilla/5.0");
+                connection.setRequestProperty("ACCEPT-LANGUAGE", "en-US,en;0.5");
+
+                int responseCode = connection.getResponseCode();
+
+                System.out.println("\nSending 'POST' request to URL : " + url);
+//                System.out.println("Post parameters : " + urlParameters);
+                System.out.println("Response Code : " + responseCode);
+
+                final StringBuilder output = new StringBuilder("Request URL " + url);
+                output.append(System.getProperty("line.separator")  + "Response Code " + responseCode);
+                output.append(System.getProperty("line.separator")  + "Type " + "GET");
+                BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line = "";
+                StringBuilder responseOutput = new StringBuilder();
+
+                while((line = br.readLine()) != null ) {
+                    responseOutput.append(line);
+                }
+                br.close();
+
+                System.out.println("output===============" + responseOutput.toString());
+                result = responseOutput.toString();
+
+                output.append(System.getProperty("line.separator") + "Response " + System.getProperty("line.separator") + System.getProperty("line.separator") + responseOutput.toString());
+
+                /*SimpleLocationDemoActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        progress.dismiss();
+
+                    }
+                });*/
+
+
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            try {
+                JSONArray jArray = new JSONArray(result);
+                for(int i=0; i < jArray.length(); i++) {
+
+                    JSONObject jObject = jArray.getJSONObject(i);
+
+                    String username = jObject.getString("username");
+                    String email = jObject.getString("email");
+                    int deviceid = jObject.getInt("deviceID");
+
+                    System.out.println("SimpleLocationDemo " + "UserName:" + username + email + deviceid);
+
+                } // End Loop
+                this.progress.dismiss();
+            } catch (JSONException e) {
+                Log.e("JSONException", "Error: " + e.toString());
+            } // catch (JSONException e)
+        }
+    }
+}
